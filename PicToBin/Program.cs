0ad30d819c4bc7691d3e8742cbe55cdb22904c05
Jon Zlotnik <me@jonzlotnik.info>
2021-11-25 13:17:19 -0500

    pic to bin file converter done

--- PicToBin/Program.cs
+++ PicToBin/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Check if args are there:
+if (args == null || args.Length != 2)
+{
+    Console.Error.WriteLine("ERROR: Not enough arguments.");
+    return 1;
+}
+
+// Check if file to create exists
+if (File.Exists(args[1]))
+{
+    Console.Error.WriteLine("ERROR: Will not overwrite file.");
+    return 1;
+}
+
+// Convert file
+Console.WriteLine("Converting file to binary...");
+
+using (var outputFile = File.Create(args[1]))
+{
+
+    if (File.Exists(args[0]))
+    {
+        using (StreamReader sr = new StreamReader(args[0]))
+        {
+            for (string? line; (line = sr.ReadLine()) != null;)
+            {
+                try
+                {
+                    int currentInteger = Int32.Parse(line);
+                    outputFile.Write(BitConverter.GetBytes(currentInteger));
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine("ERROR: Could not parse input file.");
+                    return 1;
+                }
+            }
+        }
+    }
+}
+
+return 0;
