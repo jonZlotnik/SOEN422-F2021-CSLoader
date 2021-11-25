e126dce2e3c3c577eb10512bc0f543c95c52938a
jonZlotnik <me@jonzlotnik.info>
2021-11-25 15:12:04 -0500

    Revert "Pic to bin (#1)"
    This reverts commit d67ea33145e156395ffabeda218dc09c3efca522.

--- RunPicToBin.ps1
+++
@@ -1,9 +0,0 @@
-$filesToConvert = Get-ChildItem -Path ./TestPicToBin
-Set-Location -Path PicToBin
-dotnet build -o bin
-foreach ($picFile in $filesToConvert) {
-    $inFile = $picFile.FullName
-    $outFile = "$inFile" + ".bin"
-    ./bin/PicToBin.exe "$inFile" "$outFile"
-}
-Set-Location -Path ../
